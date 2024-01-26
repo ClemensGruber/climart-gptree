601ddf0e7dcf21e6a4f68c5af35e88b8da737a81
Clemens Gruber <36139413+ClemensGruber@users.noreply.github.com>
2024-01-26 12:02:42 +0100

    adjust start.sh deactivate venv

--- start.sh
+++ start.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
 cd ~/gptree
+
+# activate virtual environment
 source venv-gptree/bin/activate
+
+# run GPTree script
 python3 main.py
+
+# deactivate virtual environment when script is finished
+deactivate
